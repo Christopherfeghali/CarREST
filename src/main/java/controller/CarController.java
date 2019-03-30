ecf9d8c54533b2754e55e90e487d756aa05a7c44
Christopher Feghali <khcy5cme@nottingham.edu.my>
2019-03-30 18:34:41 +0400

    reponsebody

--- src/main/java/controller/CarController.java
+++ src/main/java/controller/CarController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import model.Car;
@@ -15,6 +16,7 @@ import service.CarService;
 @CrossOrigin
 @RestController
 @RequestMapping("/car")
+@ResponseBody
 public class CarController {
 
 	@Autowired
